710051d19877b35ef008a7e302fa9d8b1c4cf553
kellylondon <kellyblizz343@gmail.com>
2019-07-20 02:47:06 -0700

    installation of depencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, Router, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenComponent } from './men/men.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { FooterComponent } from './footer/footer.component';
+import { CollectionsComponent } from './collections/collections.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'men',
+    component: MenComponent
+  },
+  {
+    path: 'aboutus',
+    component: AboutusComponent
+  },
+  {
+    path: 'collections',
+    component: CollectionsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutusComponent,
+    FooterComponent,
+    CollectionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
